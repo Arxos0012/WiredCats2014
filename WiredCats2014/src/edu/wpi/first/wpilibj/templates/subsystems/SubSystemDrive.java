930c516b504b478006983b921350738b96acac65
Bruce Crane <samcrane8@gmail.com>
2014-02-01 13:16:57 -0500

    Transmission Shifting code   - auto upshift code   - manual downshift. Drive Subsystem   - added speed checking functionality   - added stuff.   -  imy hands are typing woooooooords.   - sdjfkl;asdkhf;wqyrieo;

--- WiredCats2014/src/edu/wpi/first/wpilibj/templates/subsystems/SubSystemDrive.java
+++ WiredCats2014/src/edu/wpi/first/wpilibj/templates/subsystems/SubSystemDrive.java
@@ -3,6 +3,8 @@ package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Accelerometer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.templates.RobotMap;
@@ -16,10 +18,21 @@ public class SubSystemDrive extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     
+    public static final int TICKS_PER_ROTATION = 240;
+    // radius = 2, multiplied by 2pi, divided by 12 to convert to feet.
+    public static final float WHEEL_CIRCUMFERENCE_FEET = (float)((2*2*Math.PI)/12);
+    public static final float TICKS_TO_FEET = WHEEL_CIRCUMFERENCE_FEET / TICKS_PER_ROTATION;
+    
     Talon left = new Talon(RobotMap.DRIVE_LEFT);
     Talon right = new Talon(RobotMap.DRIVE_RIGHT);
     Gyro gyro = new Gyro(RobotMap.DRIVE_GYRO);
     Accelerometer accel = new Accelerometer(RobotMap.DRIVE_ACCEL);
+    Encoder leftEncoder = new Encoder(RobotMap.DRIVE_LEFT_ENCODER_A,
+                                        RobotMap.DRIVE_LEFT_ENCODER_B);
+    Encoder rightEncoder = new Encoder(RobotMap.DRIVE_RIGHT_ENCODER_A,
+                                        RobotMap.DRIVE_RIGHT_ENCODER_B);
+    Solenoid lowSpeedSolenoid = new Solenoid(RobotMap.DRIVE_LOW_SPEED_SOLENOID);
+    Solenoid highSpeedSolenoid = new Solenoid(RobotMap.DRIVE_HIGH_SPEED_SOLENOID);
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
@@ -34,6 +47,26 @@ public class SubSystemDrive extends Subsystem {
         this.right.set(-power);
     }
     
+    public void setHighSpeed(){
+        highSpeedSolenoid.set(true);
+        lowSpeedSolenoid.set(false);
+    }
+    
+    /**
+     * Returns the speed of the robot in feet per second.
+     * @return 
+     */
+    public float getSpeed(){
+        float ticks = (float)(leftEncoder.getRate() + rightEncoder.getRate());
+        ticks /= 2;
+        return TICKS_TO_FEET * ticks;
+    }
+    
+    public void setLowSpeed(){
+        lowSpeedSolenoid.set(true);
+        highSpeedSolenoid.set(false);
+    }
+    
     public float getGyroAngle(){
         return (float)gyro.getAngle();
     }
@@ -42,5 +75,8 @@ public class SubSystemDrive extends Subsystem {
         gyro.reset();
     }
     
+    public boolean isHighSpeed(){
+        return highSpeedSolenoid.get();
+    }
 }
 
